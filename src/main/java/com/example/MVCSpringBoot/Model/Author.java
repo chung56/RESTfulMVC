3997b6078ebb3c2d5725bf12455e85a89c152577
chung <chung@seznam.cz>
2021-02-17 10:45:25 +0100

    update models

--- src/main/java/com/example/MVCSpringBoot/Model/Author.java
+++ src/main/java/com/example/MVCSpringBoot/Model/Author.java
@@ -40,35 +40,4 @@ public class Author implements Serializable {
         this.bookList.add( book );
     }
 
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
-//
-//    public List<Book> getBookList() {
-//        return bookList;
-//    }
-//
-//    public void setBookList(List<Book> bookList) {
-//        this.bookList = bookList;
-//    }
 }
